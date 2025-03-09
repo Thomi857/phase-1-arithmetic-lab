4197ac3e0b77b90f6eb9412ce5e9ed843ba4b9af
CliffLinusthomi <linusthomi2@gmail.com>
2025-03-09 16:47:16 +0300

    math

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// Write your code herephase-1-arithmetic-lab
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 84;
+let num4 = 10;
+
+let multiply = (num1 * num2);
+
+console.log(multiply);
+
+
+let random = Math.floor((Math.random() * 10) + 1);
+
+let mod = (num3 % num4);
+
+let max = Math.max(6,3,7,12,11,9,20,18,19);
